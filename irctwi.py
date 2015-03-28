6d55aa30817b5ce4c879a039864ce7c8857dbd77
himaaaatti <sux2mfgj@gmail.com>
2015-03-28 17:53:47 +0900

    write

---
+++ irctwi.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+import socket
+
+def main():
+    host = '127.0.0.1'
+    port = 16668
+
+    server_sock
---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+import sys
+
+
+sys.path.append('irctwi')
+import irctwi
+
+setup(
+        name='irctwi',
+        version='0.0',
+        author='himaaaatti',
+        descripthon='',
+
+        )
